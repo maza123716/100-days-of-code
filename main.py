14b1187576f6a24bb2f5f7376c1a9272bf017fff
maza123716 <64093424+maza123716@users.noreply.github.com>
2023-04-04 17:08:27 +0300

    day 1-11

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ def add_num(val_num):
 
 
 def validate_and_execute():
-    user_input = ""  # assign an empty string to the user input
-    while user_input != "exit":  # Condition gets evaluated
-        user_input = input("Enter the value of the number\n")
-        try:  # lets you test a block of code for errors
-            if int(user_input) > 0:  # nested if
-                final_val = add_num(int(user_input))  # casting
-                print(final_val)
-            elif int(user_input) <= 0:
-                print('Enter a valid number')
-        except ValueError:  # catches the error and lets you handle it
-            print('The input is not an integer')
+    try:  # lets you test a block of code for errors
+        user_input_val = int(num_of_elements)
+        if user_input_val > 0:  # nested if
+            final_val = add_num(user_input_val)  # casting
+            print(final_val)
+        elif user_input_val <= 0:
+            print('Enter a valid number')
+    except ValueError:  # catches the error and lets you handle it
+        print('The input is not an integer')
 
 
-validate_and_execute()  # User is prompted for input
+user_input = ""  # assign an empty string to the user input
+while user_input != "exit":  # Condition gets evaluated
+    user_input = input("Enter the value of the number\n")
+    for num_of_elements in user_input.split(", "):
+        validate_and_execute()  # User is prompted for input
